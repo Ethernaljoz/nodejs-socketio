1c1ddd99cca88089baa07d8eade8037b9f02079a
Ethernaljoz <155591690+Ethernaljoz@users.noreply.github.com>
2024-12-04 17:20:39 +0000

    authenticate middleware

---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,21 @@
+import { RequestHandler } from "express";
+import mongoose from "mongoose";
+import { UNAUTHORIZED } from "../constants/httpCode";
+import { appAssert } from "../utils/AppError";
+import { verifyToken } from "../utils/jwt";
+
+
+const authenticate: RequestHandler = (req,res,next)=> {
+    const accesToken = req.cookies.accessToken as string || undefined
+
+    appAssert(accesToken, UNAUTHORIZED,"not authorized- invalid access token")
+
+    const { error, payload } = verifyToken(accesToken)
+    appAssert(error, UNAUTHORIZED,error ==="jwt expired"?"token expired":"invalid token")
+
+    req.userId = payload.userId as mongoose.Types.ObjectId,
+    req.sessionId = payload.sessionId as mongoose.Types.ObjectId
+
+    next()
+
+}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,16 @@
+import mongoose from "mongoose";
+
 export const enum verificationCodeType {
     EmailVerification = "email_verification",
     PasswordReset = "password_reset"
+}
+
+
+declare global {
+    namespace Express{
+        interface Request{
+            userId: mongoose.Types.ObjectId
+            sessionId: mongoose.Types.ObjectId 
+        }
+    }
 }
